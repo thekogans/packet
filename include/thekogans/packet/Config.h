a44f04e6cde209a985e4a3552c6d138b1d18f806
Boris Kogan <boris@thekogans.net>
2022-01-21 12:15:16 -0500

    Added environment deduction.

--- include/thekogans/packet/Config.h
+++ include/thekogans/packet/Config.h
@@ -22,6 +22,8 @@
     #error libthekogans_packet requires C++ compilation (use a .cpp suffix)
 #endif // !defined (__cplusplus)
 
+#include "thekogans/util/Environment.h"
+
 #if defined (TOOLCHAIN_OS_Windows)
     #define _LIB_THEKOGANS_PACKET_API __stdcall
     #if defined (THEKOGANS_PACKET_TYPE_Shared)
